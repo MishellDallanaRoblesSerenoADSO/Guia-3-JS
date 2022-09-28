0d746c1fff0ec200b835949d28919b58e8bed857
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-09-28 12:15:41 -0500

    Ejercicio 1

---
+++ main.js
@@ -0,0 +1,12 @@
+addEventListener("DOMContentLoaded",()=>{
+// Ejercicio 1
+    // # En una granja se requiere saber alguna información para determinar el precio de venta por cada kilo 
+    // # de huevo. precio se determina a traves del promedio de calidad de las N gallinas que hay en la
+    // # granja. La calidad de cada gallina se obtiene según la formula:
+
+    let numg=Number(prompt("Cuanta cantidad de gallina?: "))
+    n1=1
+    n2=0
+
+    
+})
