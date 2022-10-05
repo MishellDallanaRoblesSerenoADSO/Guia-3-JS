bed02310a9efdd4ccf07d4a4e95b2e81ce26bec9
MishellDallanaRoblesSerenoADSO <mishellrobles2004@gmail.com>
2022-10-04 20:26:50 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 addEventListener("DOMContentLoaded",()=>{
-// Ejercicio 2
-    //2. Hacer un algoritmo que al ingresar 2 números por la pantalla y se calcule la suma, resta, multiplicación
-    //y división. El proceso debe terminar cuando se hallan realizado 10 procesos (Hacer uso de
-    //contadores).
+// Ejercicio 3
+    //Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de computación.
     
-    n=0
-    while (n<10){
-        num1=Number(prompt("Ingrese el numero 1: "))
-        num2=Number(prompt("Ingrese el numero 2: "))
+    d=0
+    while (d<5){
+        d++
+        nom=prompt("Ingrese su Nombre y Apellido: ")
+        edad=prompt("Ingrese su Edad: ")
+        n1=Number(prompt("Coloque la nota 1: "))
+        n2=Number(prompt("Coloque la nota 2: "))
+        n3=Number(prompt("Coloque la nota 3: "))
         
-        suma=num1+num2
-        rest=num1-num2 
-        multi=num1*num2 
-        div=num1/num2
-        
-        alert(`La suma de ${num1} + ${num2} es= ${suma}`)
-        alert(`La resta de ${num1} - ${num2} es= ${rest}`)
-        alert(`la multiplicacion de ${num1} * ${num2} es= ${multi}`)
-        alert(`La division de ${num1} / ${num2} es= ${div}`)
-        n+=1
+        prom=(n1+n2+n3)/3
 
+        console.log(`El promedio del estudiante ${nom} con ${edad} años de edad es: ${prom} `)
     }
 })
